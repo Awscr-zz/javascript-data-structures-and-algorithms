b36c16a194242939dd977442d563caed01520dd9
Oscar Serrato <s.awscr@gmail.com>
2020-07-06 18:55:00 -0700

    Add Recursion/flatten, is_palindrome, reverse, some_recursive

---
+++ Recursion/reverse.js
@@ -0,0 +1,14 @@
+/* Reverse - Recursion
+
+Instructions: 
+- Write a recursive function called reverse which accepts a string and returns a new string in reverse */
+
+function reverse(str) {
+  if (str.length <= 1) return str;
+
+  return reverse(str.slice(1)) + str[0];
+}
+
+// TESTS
+console.log(reverse('awesome')); //'emosewa'
+console.log(reverse('cool')); //'looc'
